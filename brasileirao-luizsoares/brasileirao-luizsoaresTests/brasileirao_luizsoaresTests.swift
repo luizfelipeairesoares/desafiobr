1745d8da9c47ef8742c5b37e37c83d631ea350d4
luizfelipeairesoares <lfelipeas@gmail.com>
2019-01-05 16:47:57 -0200

    New project

---
+++ brasileirao-luizsoares/brasileirao-luizsoaresTests/brasileirao_luizsoaresTests.swift
@@ -0,0 +1,34 @@
+//
+//  brasileirao_luizsoaresTests.swift
+//  brasileirao-luizsoaresTests
+//
+//  Created by Luiz Felipe Aires Soares on 05/01/19.
+//  Copyright © 2019 globo. All rights reserved.
+//
+
+import XCTest
+@testable import brasileirao_luizsoares
+
+class brasileirao_luizsoaresTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
